681c04b0bb09eaecea3fa39a703af87779a4a950
ADITHYAN <cb4103317@gmail.com>
2025-11-20 22:51:41 -0800

    Update config.js

--- config.js
+++ config.js
@@ -30,10 +30,10 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "us-01.strixnodes.com", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 8003,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
-                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "zac.hidencloud.com", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 24627,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "Kaun.Yuvraj", 
+                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'false' || true
             },           
             
             /**
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
