29410eebffb53889da49351ff98212ebdd2b3bd6
ADITHYAN <cb4103317@gmail.com>
2026-01-05 00:20:04 -0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * @fileoverview 
  * @module ConfigurationManager
  * @version 1.0.0
- * @author GlaceYT
+ * @author Rick, Adhidhi
  */
 
 const EnvironmentVariableProcessor = require('process').env;
@@ -100,6 +100,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
