4e41a9c149a0c36d5301e3c0a320e2a2cf0fd162
ADITHYAN <cb4103317@gmail.com>
2026-01-05 00:56:57 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-/**
- * Ultimate Music Bot - Core Application
- * @version 1.0.5
- */
-
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { Riffy } = require('riffy');
 const fs = require('fs');
@@ -10,10 +5,7 @@ const path = require('path');
 const config = require('./config');
 const DatabaseConnection = require('./database/connection');
 const AudioPlayerHandler = require('./utils/player');
-const StatusManager = require('./utils/statusManager');
-const MemoryOptimizer = require('./utils/garbageCollector');
 require('dotenv').config();
-const shiva = require('./shiva');
 
 class MusicBot {
     constructor() {
@@ -29,12 +21,11 @@ class MusicBot {
         this.client.commands = new Collection();
         this.client.slashCommands = new Collection();
         
+        // Setup Audio Infrastructure
         this.initializeAudio();
-        this.setupSystems();
     }
 
     initializeAudio() {
-        // Construct the nodes exactly as Riffy expects
         const nodes = [{
             name: "Main-Node",
             host: config.lavalink.host,
@@ -43,7 +34,6 @@ class MusicBot {
             secure: config.lavalink.secure === true || config.lavalink.secure === "true"
         }];
 
-        // Assign riffy to the client
         this.client.riffy = new Riffy(this.client, nodes, {
             send: (payload) => {
                 const guild = this.client.guilds.cache.get(payload.d.guild_id);
@@ -52,56 +42,31 @@ class MusicBot {
             defaultSearchPlatform: "ytmsearch",
             restVersion: "v4"
         });
+
+        this.client.playerHandler = new AudioPlayerHandler(this.client);
     }
 
-    setupSystems() {
-        this.client.statusManager = new StatusManager(this.client);
-        this.client.playerHandler = new AudioPlayerHandler(this.client);
+    async start() {
+        await DatabaseConnection();
         
-        // Essential: Forward raw gateway packets to Riffy for voice functionality
-        this.client.on('raw', (d) => {
+        // Load Events
+        const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+        for (const file of eventFiles) {
+            const event = require(`./events/${file}`);
+            if (event.once) this.client.once(event.name, (...args) => event.execute(...args, this.client));
+            else this.client.on(event.name, (...args) => event.execute(...args, this.client));
+        }
+
+        // Forward raw packets for voice functionality
+        this.client.on('raw', d => {
             if (['VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE'].includes(d.t)) {
                 this.client.riffy.updateVoiceState(d);
             }
         });
-    }
 
-    async start() {
-        try {
-            await DatabaseConnection();
-            MemoryOptimizer.init();
-            this.loadModules();
-            
-            this.client.playerHandler.initializeEvents();
-            
-            // Log in to Discord
-            await this.client.login(config.discord.token || process.env.TOKEN);
-            shiva.initialize(this.client);
-        } catch (error) {
-            console.error('❌ Critical failure during startup:', error);
-        }
-    }
-
-    loadModules() {
-        // Load event handlers
-        const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-        for (const file of eventFiles) {
-            const event = require(`./events/${file}`);
-            if (event.once) {
-                this.client.once(event.name, (...args) => event.execute(...args, this.client));
-            } else {
-                this.client.on(event.name, (...args) => event.execute(...args, this.client));
-            }
-        }
-
-        // Load slash commands into collection
-        const slashFiles = fs.readdirSync('./commands/slash').filter(file => file.endsWith('.js'));
-        for (const file of slashFiles) {
-            const command = require(`./commands/slash/${file}`);
-            this.client.slashCommands.set(command.data.name, command);
-        }
+        this.client.playerHandler.initializeEvents();
+        await this.client.login(config.discord.token || process.env.TOKEN);
     }
 }
 
-const bot = new MusicBot();
-bot.start();
+new MusicBot().start();
